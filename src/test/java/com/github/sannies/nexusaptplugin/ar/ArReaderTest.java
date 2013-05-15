629ece349afb3ae6e115def9bd357b6922b4d497
Christoph Emmersberger <cemmersb@googlemail.com>
2013-05-15 16:45:40 +0200

    +/- cleanup

--- src/test/java/com/github/sannies/nexusaptplugin/ar/ArReaderTest.java
+++ src/test/java/com/github/sannies/nexusaptplugin/ar/ArReaderTest.java
@@ -1,20 +1,23 @@
 package com.github.sannies.nexusaptplugin.ar;
 
-
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ArReaderTest {
 
-    @Test
-    public void test() throws IOException {
-        File deb = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getFile(), "php5_5.3.10-1ubuntu3.2_all.deb");
-        ArReader arReader = new ArReader(deb);
-        for (ReadableArFile readableArFile : arReader) {
-            System.err.println(readableArFile.getName());
-             readableArFile.open();
-        }
+  private final static Logger LOGGER = LoggerFactory.getLogger(ArReaderTest.class);
+
+  @Test
+  public void test() throws IOException {
+    File deb = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getFile(), "php5_5.3.10-1ubuntu3.2_all.deb");
+    ArReader arReader = new ArReader(deb);
+    for (ReadableArFile readableArFile : arReader) {
+      LOGGER.error("Could not read file with name: {}", readableArFile.getName());
+      readableArFile.open();
     }
+  }
 }
